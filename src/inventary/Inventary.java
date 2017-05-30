988e422c1ce59ec1e9d29995d2f5d176b9cf3277
Omar1123 <omar1123veliz@gmail.com>
2017-05-29 20:40:11 -0600

    ADD: All order per day and general

--- src/inventary/Inventary.java
+++ src/inventary/Inventary.java
@@ -5,9 +5,11 @@
  */
 package inventary;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.OutputStreamWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -22,7 +24,7 @@ public class Inventary {
     private File ARTICLES = new File("/Users/jake/NetBeansProjects/Inventary/src/inventary/ART.txt");
     private File ORDERS = new File("/Users/jake/NetBeansProjects/Inventary/src/inventary/ORD.txt");
     private File DELIVERIES = new File("/Users/jake/NetBeansProjects/Inventary/src/inventary/DEL.txt");
-    private File DEVOLUTION = new File("");
+    private File DEVOLUTION = new File("/Users/jake/NetBeansProjects/Inventary/src/inventary/DEV.txt");
     
     /**
      * @param args the command line arguments
@@ -73,6 +75,8 @@ public class Inventary {
     public void newDeliveries(String id, String orderId, String status) {
               
         try {
+            
+            status = "INICIADO";
                    
             BufferedWriter deliveries =new BufferedWriter(new OutputStreamWriter(new FileOutputStream(DELIVERIES, true), "utf-8"));            
             
@@ -82,7 +86,100 @@ public class Inventary {
           //Captura un posible error le imprime en pantalla 
             System.out.println(ex.getMessage());
         }          
-    }       
+    }     
+    
+    public void newDevolution(String id, String idOldArticle, String idNewArticle) {
+              
+        try {
+                   
+            BufferedWriter devolution =new BufferedWriter(new OutputStreamWriter(new FileOutputStream(DEVOLUTION, true), "utf-8"));            
+            
+            devolution.write(id + "," + idOldArticle + "," + idNewArticle + "," + getDate() + "\r\n");                                                            
+            devolution.close();                                  
+        } catch (Exception ex) {
+          //Captura un posible error le imprime en pantalla 
+            System.out.println(ex.getMessage());
+        }          
+    } 
+    
+    public void allOrders() {
+        
+        try {            
+            if(ORDERS.exists()){
+                
+                BufferedReader buffer = new BufferedReader(new FileReader(ORDERS));                
+                String lines;
+                                
+                while((lines = buffer.readLine())!= null) {                
+                    System.out.println("Id: " + lines.split(",")[0]);
+                    System.out.println("Order: " + lines.split(",")[1]);
+                    System.out.println("Client: " + lines.split(",")[2]);
+                    System.out.println("Direction: " + lines.split(",")[3]);
+                    System.out.println("Phone: " + lines.split(",")[4]);
+                    System.out.println("NIT: " + lines.split(",")[5]);
+                    System.out.println("Article: " + lines.split(",")[6]);
+                    System.out.println("Amount: " + lines.split(",")[7]);
+                    System.out.println("Price: " + lines.split(",")[8]);
+                    System.out.println("Date: " + lines.split(",")[9]);
+                }
+                
+                buffer.close();
+              }else{
+                System.out.println("Fichero No Existe");
+              }
+        } catch (Exception ex) {
+            /*Captura un posible error y le imprime en pantalla*/ 
+             System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void allOrdersPerDay(String date) {
+        try {            
+            if(ORDERS.exists()){
+                
+                BufferedReader buffer = new BufferedReader(new FileReader(ORDERS));                
+                String lines;
+                
+                while((lines = buffer.readLine())!= null) {
+                    if(lines.indexOf(",")!= -1){
+                        if (lines.split(",")[9].equalsIgnoreCase(date)) {                        
+                            System.out.println("Id: " + lines.split(",")[0]);
+                            System.out.println("Order: " + lines.split(",")[1]);
+                            System.out.println("Client: " + lines.split(",")[2]);
+                            System.out.println("Direction: " + lines.split(",")[3]);
+                            System.out.println("Phone: " + lines.split(",")[4]);
+                            System.out.println("NIT: " + lines.split(",")[5]);
+                            System.out.println("Article: " + lines.split(",")[6]);
+                            System.out.println("Amount: " + lines.split(",")[7]);
+                            System.out.println("Price: " + lines.split(",")[8]);
+                            System.out.println("Date: " + lines.split(",")[9]);
+                        } 
+                    }
+                }                                               
+                
+                buffer.close();
+              }else{
+                System.out.println("Fichero No Existe");
+              }
+        } catch (Exception ex) {
+            /*Captura un posible error y le imprime en pantalla*/ 
+             System.out.println(ex.getMessage());
+        }
+    }
+    
+    public void allOrdersByStatus(String status) {
+        
+    }
+    
+    public void allSustitutions() {
+        
+    }
+    
+    public void generalReport() {
+        
+    }
+    
+    
     
     public String getDate() {
         
